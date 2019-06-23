5a78da2e92f45815e1c2cca2b1a2dc842f492c49
liuguojun1022 <l666_6@126.com>
2019-06-23 10:46:10 +0800

    刘刘刘

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+// 设置 默认采用的模板引擎名称
+app.set('view engine', 'ejs')
+    // 设置模板页面的存放路径
+app.set('views', './views')
+
+app.use('/node_modules', express.static('node_modules'))
+
+
+app.get('/', (req, res) => {
+    res.render('index', {})
+})
+
+app.get('/login', (req, res) => {
+    res.render('./user/login.ejs', {})
+})
+
+app.get('register', (req, res) => {
+    res.render('./user/register.ejs', {})
+})
+
+app.listen(80, () => {
+    console.log("服务器运行成功……")
+})
